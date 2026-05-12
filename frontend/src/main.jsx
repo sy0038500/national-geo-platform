b4b591e979b4d29e5c156c9063ea5896a32d6284
Lee Sooyeon <sy.0038500@gmail.com>
2026-05-13 00:44:26 +0900

    fix: add basename to BrowserRouter for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/national-geo-platform">
       <App />
     </BrowserRouter>
   </React.StrictMode>
